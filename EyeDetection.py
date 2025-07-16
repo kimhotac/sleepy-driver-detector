2b06d8dab0db9acf0e58edd91a5ddd5f065f92e0
kimhotac <114737603+kimhotac@users.noreply.github.com>
2025-07-16 11:16:28 +0900

    클래스화

--- EyeDetection.py
+++ EyeDetection.py
@@ -16,7 +16,7 @@ class EyeDetector:
         self.RIGHT_EYE_IDX = [33, 133, 160, 159, 158, 157, 173, 246]
         self.LEFT_EYE_IDX  = [362, 263, 387, 386, 385, 384, 398, 466]
 
-    def get_eye_bounding_boxes(self, frame):
+    def get_bounding_boxes(self, frame):
         '''
         이미지 프레임에서 얼굴을 탐지하고, 
         왼쪽/오른쪽 눈의 bounding box 좌표를 반환합니다.
@@ -52,3 +52,29 @@ class EyeDetector:
         left_bbox = cv2.boundingRect(left_eye_points)  # (x, y, w, h)
 
         return right_bbox, left_bbox
+
+    def crop_and_resize(self, frame, bbox, size=90):
+        """
+        주어진 프레임에서 bounding box 영역(눈 부분)을 주변 여유 공간과 함께 잘라내어,
+        지정한 크기의 정사각형 이미지로 리사이즈합니다.
+
+        Args:
+            frame (np.ndarray): 입력 이미지 (OpenCV BGR 형식).
+            bbox (tuple): (x, y, w, h) 형태의 bounding box 좌표.
+            size (int, optional): 반환할 정사각형 이미지의 한 변 크기. 기본값은 90.
+
+        Returns:
+            np.ndarray or None: 크롭 및 리사이즈된 눈 이미지 (shape: [size, size, 3]).
+                bbox가 None이면 None을 반환합니다.
+        """
+        if bbox is None:
+            return None
+
+        x, y, w, h = bbox
+        x1 = max(x - h//2, 0)
+        y1 = max(y - w//2, 0)
+        x2 = min(x + w + h//2, frame.shape[1])
+        y2 = min(y + h + w//2, frame.shape[0])
+        eye_crop = frame[y1:y2, x1:x2]
+        eye_resized = cv2.resize(eye_crop, (size, size))
+        return eye_resized
