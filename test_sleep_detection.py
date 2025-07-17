48bd7892a59770dc2d4bd4fea52b99239e0ec58e
박준규 <junju404@naver.com>
2025-07-17 13:47:50 +0900

    feat: 모델별 클래스 관심사 분리하도록 구현

---
+++ test_sleep_detection.py
@@ -0,0 +1,21 @@
+from SleepDetection.SleepDetection import SleepDetection
+
+# OpenCV 모델 사용 예제
+OECD = SleepDetection('Opencv')
+OECD.run()
+
+# InfraredMLP 모델 사용 예제
+# IFMLP = SleepDetection('InfraredMLP')
+# IFMLP.run()
+
+# MLEye 모델 사용 예제
+# MLEye = SleepDetection('MLEye')
+# MLEye.run()
+
+# MLP 모델 사용 예제
+# MLP = SleepDetection('MLP')
+# MLP.run()
+
+# Point 모델 사용 예제
+# Point = SleepDetection('Point')
+# Point.run() 
